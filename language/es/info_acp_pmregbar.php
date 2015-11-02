0b284f174aff75152e421b977350450fd887e45d
dmzx <info@dmzx-web.net>
2015-11-02 18:08:33 +0100

    Add language

---
+++ language/es/info_acp_pmregbar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package phpBB Extension - PM Notify & Guest Register bar
+* @copyright (c) 2015 dmzx - http://www.dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @Author Stoker - http://www.phpbb3bbcodes.com
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_PMREGBAR'						=> 'PM Notify & Guest Register bar',
+	'ACP_PMREGBAR_CONFIG_SETTINGS'		=> 'Ajustes',
+	'ACP_PMREGBAR_CONFIG_SET'			=> 'Configuración',
+));
