c55355af12795905bfb6e6b75bd188505666778d
dmzx <info@dmzx-web.net>
2019-12-23 13:52:22 +0100

    Version 1.0.6

--- migrations/pmregbar_module.php
+++ migrations/pmregbar_module.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB Extension - PM Notify & Guest Register bar
-* @copyright (c) 2015 dmzx - http://www.dmzx-web.net
+* @copyright (c) 2015 dmzx - https://www.dmzx-web.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 * @Author Stoker - http://www.phpbb3bbcodes.com
 *
